5a95e30cb35d0bc8df7b3634e61be66d62077957
garug <allefff@gmail.com>
2022-10-27 17:00:47 -0300

    Added some tests and start dev

--- src/modulo_3/logic.clj
+++ src/modulo_3/logic.clj
@@ -1,1 +1,12 @@
 (ns modulo-3.logic)
+
+(defn retirada-bike [id-bike id-usuario db]
+  ;; retira point da bike
+  ;; adiciona o user a bike
+)
+
+(defn devolucao-bike [id-bike id-ponto db]
+  ;; verifica se o ponto tem vaga disponivel
+  ;; retira user da bike
+  ;; adiciona o point a bike
+)
--- test/modulo_3/logic_test.clj
+++ test/modulo_3/logic_test.clj
@@ -1,2 +1,18 @@
 (ns modulo-3.logic-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [modulo-3.logic :refer :all]))
+
+(def db (atom {:users {1 {} 2 {}}
+               :bikes {1 {:point 1} 2 {:user 2}}}))
+
+(deftest retirada-bike-test
+  (testing "Retirada da bike foi um sucesso"
+    (is (= {:users {1 {}}
+            :bikes {:user 1}} (retirada-bike 1 1 @db))))
+
+  ;; ih rapaz, ver como testa o erro
+  (testing "Bike ta com outro usuario"
+    (retirada-bike 2 1 @db))
+
+  (testing "Usuário já possui bike vinculada"
+    (retirada-bike 1 2 @db)))
