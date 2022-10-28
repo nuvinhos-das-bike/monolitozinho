2538a265cd62523c4f0961082f75117a41bc4284
Bruno Alves <46243765+brunoalvesufu@users.noreply.github.com>
2022-10-28 13:46:04 -0300

    Adicionada rota de retirada

--- src/modulo_3/routing.clj
+++ src/modulo_3/routing.clj
@@ -4,7 +4,8 @@
 
 (def routes
   (route/expand-routes
-    #{["/hello" :get (fn [request] {:status 200 :body "hello world"}) :route-name :hello]}))
+    #{["/hello" :get (fn [request] {:status 200 :body "hello world"}) :route-name :hello]
+      ["/bikes/:id-bike/users/:id" :post logic/retirada-bike :route-name :retirada]}))
 
 (defrecord Routes []
   component/Lifecycle
