230038f967e07d79c09e716ee14bc9f7232f2c80
Rodrigo Furtado <rodrigofurtado.8.8@gmail.com>
2022-10-28 11:41:58 -0300

    schemas started

--- src/modulo_3/interceptors.clj
+++ src/modulo_3/interceptors.clj
@@ -1,9 +1,10 @@
 (ns modulo-3.interceptors
-  (:require [io.pedestal.interceptor :as i]))
+  (:require [io.pedestal.interceptor :as i]
+            [modulo-3.logic :as l]))
 
 (def all-points-interceptor
   (i/interceptor {:name  :all-points-interceptor
                   :enter (fn [context]
                            (let [db (:db context)]
                              (assoc context :response {:status 200
-                                                       :body   (get @db :points)})))}))
+                                                       :body   (l/all-points @db)})))}))
--- src/modulo_3/logic.clj
+++ src/modulo_3/logic.clj
@@ -1,1 +1,4 @@
 (ns modulo-3.logic)
+
+(defn all-points [db]
+  (get :points db))
