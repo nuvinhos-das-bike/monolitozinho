ca0a465683463b636cce90afd02f47ba180f7f86
Daniel Marques <danielmarques7@hotmail.com>
2022-10-28 15:04:39 -0300

    change get-point route flow

--- src/modulo_3/routing.clj
+++ src/modulo_3/routing.clj
@@ -1,31 +1,15 @@
 (ns modulo-3.routing
   (:require [io.pedestal.http.route :as route]
             [com.stuartsierra.component :as component]
-            [clojure.pprint :as pprint]
-            [modulo-3.interceptors :as i]))
-
-(def get-point-id-and-points
-  {:name  ::get-point
-   :enter (fn
-            [{:keys [request db] :as context}]
-            (let [path-params (-> request :path-params)
-                  id (:id path-params)
-                  points (-> @db :points)]
-              (update context :request #(assoc % :points points
-                                                 :id id))))})
-
-(defn get-point
-  [{:keys [points id]}]
-  (let [point (get points (keyword (str id)))]
-    {:status 200
-     :body   (str point)}))
+            [modulo-3.interceptors :as i]
+            [modulo-3.logic :as l]))
 
 (def routes
   (route/expand-routes
     #{["/hello" :get (fn [request] {:status 200 :body "hello world"}) :route-name :hello]
       ["/points" :get i/all-points-interceptor :route-name :all-points]
       ["/points/:id"
-       :get [get-point-id-and-points get-point]
+       :get [i/get-point-interceptor l/get-point]
        :route-name :get-point]}))
 
 (defrecord Routes []
