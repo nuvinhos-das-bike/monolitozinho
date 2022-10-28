c63a4256ed30d9689520174220f4c2cf5416921b
Bruno Alves <46243765+brunoalvesufu@users.noreply.github.com>
2022-10-28 13:30:06 -0300

    Update logic.clj

--- src/modulo_3/logic.clj
+++ src/modulo_3/logic.clj
@@ -1,9 +1,62 @@
 (ns modulo-3.logic)
 
-(defn retirada-bike [id-bike id-usuario db]
-  ;; retira point da bike
-  ;; adiciona o user a bike
-)
+(def db-provisorio (atom
+                     {:points {:1 {:name     "ponto da felicidade"
+                                   :capacity 10
+                                   :address  {:street       "rua da felicidade"
+                                              :number       "12a"
+                                              :zip-code     "72000000"
+                                              :address-line ""}}
+                               :2 {:name     "ponto da euforia"
+                                   :capacity 50
+                                   :address  {:street       "rua da felicidade"
+                                              :number       "12a"
+                                              :zip-code     "72000000"
+                                              :address-line ""}}}
+                      :bikes  {:1 {:point 1
+                                   :user  nil}
+                               :2 {:point 1
+                                   :user  nil}
+                               :3 {:point 2
+                                   :user  nil}
+                               :4 {:point nil
+                                   :user  1}}
+                      :users  {:1 {:login      "admin"
+                                   :subscribed true
+                                   :key        "/&[3-.wff@qx'{aTX-2P>}XE_B6Jc+"}}}))
+
+(defn retirada-bike [request]
+  (let [id-bike (-> request :path-params :id-bike keyword)
+        id-user (-> request :path-params :id keyword)
+        bike (-> @db-provisorio :bikes id-bike)]
+    ;(println bike)
+
+    (if (= id-user (:user bike))
+      (throw (Exception. "Bike já vinculada ao usuário.")))
+
+    (if (:user bike)
+      (throw (Exception. "Bike vinculada a outro usuário.")))
+
+    ;; retira point da bike
+    (swap!
+      db-provisorio
+      assoc-in [:bikes id-bike :point] nil)
+
+    ;; adiciona o user a bike
+    (swap!
+      db-provisorio
+      assoc-in [:bikes id-bike :user] id-user)
+
+    ;(println (-> @db-provisorio
+    ;             :bikes
+    ;             id-bike))
+    ;(println @db-provisorio)
+    {:status 200 :body {:id-bike id-bike
+                        :id-user id-user
+                        :id-point (-> @db-provisorio
+                                      :bikes
+                                      id-bike
+                                      :point)}}))
 
 (defn has-it-capacity? [db id-ponto]
   (let [vals-of-bikes (vals (get db :bikes))
