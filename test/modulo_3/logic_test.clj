34eadf3f140e3de357fe7cb23883aaa1d3f64449
Allef Santana <allefff@gmail.com>
2022-10-27 21:56:07 -0300

    Update tests

--- test/modulo_3/logic_test.clj
+++ test/modulo_3/logic_test.clj
@@ -1,18 +1,41 @@
 (ns modulo-3.logic-test
   (:require [clojure.test :refer :all]
-            [modulo-3.logic :refer :all]))
+            [modulo-3.logic :refer :all])
+  (:import (clojure.lang ExceptionInfo)))
 
-(def db (atom {:users {1 {} 2 {}}
-               :bikes {1 {:point 1} 2 {:user 2}}}))
+(def db (atom {:users {:1 {} :2 {}}
+               :bikes {:1 {:point :1} :2 {:user :2}}}))
 
 (deftest retirada-bike-test
-  (testing "Retirada da bike foi um sucesso"
-    (is (= {:users {1 {}}
-            :bikes {:user 1}} (retirada-bike 1 1 @db))))
+  (testing "Retrieve bike successfully"
+    (is (= (retirada-bike :1 :1 @db) {:users {:1 {}}
+                                      :bikes {:user :1}})))
 
-  ;; ih rapaz, ver como testa o erro
-  (testing "Bike ta com outro usuario"
-    (retirada-bike 2 1 @db))
+  ;; TODO testes ainda podem ser melhorados
+  (testing "Bike with another user"
+    (is (thrown? Exception (retirada-bike :2 :1 @db))))
 
   (testing "Usuário já possui bike vinculada"
-    (retirada-bike 1 2 @db)))
+    (is (thrown? Exception (retirada-bike :1 :2 @db)))))
+
+(deftest bike-devolution-test
+  (testing "Bike devolution succesfully"
+    (let [db {:points {:1 {:capacity 1}}
+              :bikes  {:2 {:user :3}}}
+          expected {:points {:1 {:capacity 1}}
+                    :bikes  {:2 {:point :1}}}
+          actual (bike-devolution :2 :1 db)]
+      (is (= actual expected)))
+    (let [db {:points {:bar {:capacity 50}}
+              :bikes  {:foo {:user :foobar}}}
+          expected {:points {:bar {:capacity 50}}
+                    :bikes  {:foo {:point :bar}}}
+          actual (bike-devolution :foo :bar db)]
+      (is (= actual expected))))
+
+  (testing "Exceed point capacity"
+    (are [db] (thrown-with-msg? ExceptionInfo #"point-full" (bike-devolution :2 :1 db))
+              {:points {:1 {:capacity 1}}
+               :bikes  {:2 {:user :3} :4 {:point :1}}}
+              {:points {:1 {:capacity 1}}
+               :bikes  {:2 {:user :3} :4 {:point :1} :5 {:point :1}}})))
