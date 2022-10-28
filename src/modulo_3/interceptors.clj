4d3490aba11e26b12f4f6331234fc28f81655452
Daniel Marques <danielmarques7@hotmail.com>
2022-10-28 15:03:04 -0300

    create get-point-interceptor interceptor

--- src/modulo_3/interceptors.clj
+++ src/modulo_3/interceptors.clj
@@ -7,4 +7,11 @@
                   :enter (fn [context]
                            (let [db (:db context)]
                              (assoc context :response {:status 200
-                                                       :body (l/all-points @db)})))}))
+                                                       :body   (l/all-points @db)})))}))
+
+(def get-point-interceptor
+  (i/interceptor {:name  :get-point
+                  :enter (fn
+                           [{:keys [request db] :as context}]
+                           (let [point-id (get-in request [:path-params :id])]
+                             (update context :request #(assoc % :db @db :point-id point-id))))}))
