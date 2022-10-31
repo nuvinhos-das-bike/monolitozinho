dbd2e0b44af5f962ba49a077c4fa3b7ecd089e13
garug <allefff@gmail.com>
2022-10-31 14:53:29 -0300

    Fix on exception tests

--- test/modulo_3/logic_test.clj
+++ test/modulo_3/logic_test.clj
@@ -9,27 +9,30 @@
                :points {:1 {:street "rua da felicidade"}
                         :2 {:street "rua da felicidade 2"}}}))
 
-;(deftest bike-devolution-test
-;  (testing "Bike devolution succesfully"
-;    (let [db {:points {:1 {:capacity 1}}
-;              :bikes  {:2 {:user :3}}}
-;          expected {:points {:1 {:capacity 1}}
-;                    :bikes  {:2 {:point :1}}}
-;          actual (bike-devolution :2 :1 db)]
-;      (is (= actual expected)))
-;    (let [db {:points {:bar {:capacity 50}}
-;              :bikes  {:foo {:user :foobar}}}
-;          expected {:points {:bar {:capacity 50}}
-;                    :bikes  {:foo {:point :bar}}}
-;          actual (bike-devolution :foo :bar db)]
-;      (is (= actual expected))))
-;
-;  (testing "Exceed point capacity"
-;    (are [db] (thrown-with-msg? ExceptionInfo #"point-full" (bike-devolution :2 :1 db))
-;              {:points {:1 {:capacity 1}}
-;               :bikes  {:2 {:user :3} :4 {:point :1}}}
-;              {:points {:1 {:capacity 1}}
-;               :bikes  {:2 {:user :3} :4 {:point :1} :5 {:point :1}}})))
+(deftest bike-devolution-test
+  (testing "Bike devolution succesfully"
+    (let [db {:points {:1 {:capacity 1}}
+              :bikes  {:2 {:user :3}}}
+          expected {:points {:1 {:capacity 1}}
+                    :bikes  {:2 {:point :1}}}
+          actual (bike-devolution :2 :1 db)]
+      (is (= actual expected)))
+    (let [db {:points {:bar {:capacity 50}}
+              :bikes  {:foo {:user :foobar}}}
+          expected {:points {:bar {:capacity 50}}
+                    :bikes  {:foo {:point :bar}}}
+          actual (bike-devolution :foo :bar db)]
+      (is (= actual expected))))
+
+  (testing "Exceed point capacity"
+    (are [db] (try
+                (bike-devolution :2 :1 db)
+                (catch ExceptionInfo ex
+                  (is (= (ex-data ex) { :cause "point-full" }))))
+              {:points {:1 {:capacity 1}}
+               :bikes  {:2 {:user :3} :4 {:point :1}}}
+              {:points {:1 {:capacity 1}}
+               :bikes  {:2 {:user :3} :4 {:point :1} :5 {:point :1}}})))
 
 (deftest has-it-capacity?-test
   (let [mapa {:points {:1 {:capacity 1} :2 {:capacity 1}}
